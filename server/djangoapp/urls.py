c425e703462cbb5137ee9975d99263bc2d566055
lamyprojects <lamynessa23@gmail.com>
2026-03-04 10:39:00 +0000

    save

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
 from . import views
 
-app_name = 'djangoapp'
+app_name = "djangoapp"
 
 urlpatterns = [
-    path('analyze', views.analyze_review, name='analyze_review'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("login", views.login_user, name="login_user"),
+    path("analyze", views.analyze_review, name="analyze_review"),
+]
